9fcfbeda09ada7cd4a02cf96cf17f19e9cb83f24
Diego Freniche Brito <diego@freniche.com>
2019-07-31 23:56:13 +0200

    Put presentation in separate file, not reading file (because don't know how to open a file with CPCT)

---
+++ c/rpg/src/present.c
@@ -0,0 +1,28 @@
+#include "present.h"
+#include "util.h"
+
+void show_presentation() {
+   // int c;
+   // FILE *file;
+   // file = fopen("present.txt", "r");
+   // if (file) {
+   //    while ((c = getc(file)) != EOF)
+   //       putchar(c);
+   //    fclose(file);
+   // }
+
+    char *s = 
+    "8888888b.  8888888b.   .d8888b. \r\n"
+    "888   Y88b 888   Y88b d88P  Y88b\r\n"
+    "888    888 888    888 888    888\r\n" 
+    "888   d88P 888   d88P 888       \r\n" 
+    "8888888P   8888888P   888  88888\r\n" 
+    "888 T88b   888        888    888\r\n" 
+    "888  T88b  888        Y88b  d88P\r\n" 
+    "888   T88b 888         Y8888P88 \r\n"; 
+
+    puts(s);
+    puts("RPG GAME by dfreniche\r\n");
+
+    repeat_print("_", 10);
+}
